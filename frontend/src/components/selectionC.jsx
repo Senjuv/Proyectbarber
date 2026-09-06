17dc0e0809d72c0a5697db357e828955765a19ff
Senju1615 <170354763+Senjuv@users.noreply.github.com>
2026-09-05 21:08:48 -0600

    modificacion popup y alerts

--- frontend/src/components/selectionC.jsx
+++ frontend/src/components/selectionC.jsx
@@ -8,7 +8,7 @@ import { useReserva } from "../utils/declarations.jsx";
 
 function Selectionc() {
 
-    
+
 
     const { isOpen, setIsOpen, setDatosCita, datosCita, env, setEnv } = useReserva();
 
@@ -16,85 +16,69 @@ function Selectionc() {
         setDatosCita({ ...datosCita, corte: corte });
     };
     const dataD = (hora, id) => {
-        setDatosCita({ ...datosCita, hora: hora, id: id});
+        setDatosCita({ ...datosCita, hora: hora, id: id });
     };
 
     const dataUS = (nombre, numero) => {
         setDatosCita({ ...datosCita, nombre: nombre, numero: numero })
     }
 
-    
-    
+
+
 
     console.log(isOpen);
     return (
         <div className="section-c">
-            <h3>Catalogo de servicios</h3>
+            <h3 className="section-title">Catálogo de servicios</h3>
+
+            {/* Contenedor de ÚNICAMENTE las tarjetas */}
             <div className="container-c">
                 <div className="container-cp" id="CorteH" onClick={() => { dataC("CorteH"); setIsOpen('dia') }}>
-                    <h3>Corte de cabello (hombre)</h3>
-                    <p>$130</p>
+                    <span className="service-name">Corte de cabello (Hombre)</span>
+                    <span className="service-price">$130</span>
                 </div>
+
                 <div className="container-cp" id="CorteM" onClick={() => { dataC("CorteM"); setIsOpen('dia') }}>
-                    <h3>Corte de cabello (Mujer)</h3>
-                    <p>$130</p>
+                    <span className="service-name">Corte de cabello (Mujer)</span>
+                    <span className="service-price">$130</span>
                 </div>
+
                 <div className="container-cp" id="Barba" onClick={() => { dataC("Barba"); setIsOpen('dia') }}>
-                    <h3>Barba (Incluye spa)</h3>
-                    <p>$100</p>
+                    <span className="service-name">Barba (Incluye spa)</span>
+                    <span className="service-price">$100</span>
                 </div>
+
                 <div className="container-cp" id="Ceja" onClick={() => { dataC("Ceja"); setIsOpen('dia') }}>
-                    <h3>Ceja</h3>
-                    <p>$50</p>
+                    <span className="service-name">Ceja</span>
+                    <span className="service-price">$50</span>
                 </div>
+
                 <div className="container-cp" id="GrecaS" onClick={() => { dataC("GrecaS"); setIsOpen('dia') }}>
-                    <h3>Greca sencilla</h3>
-                    <p>$30</p>
+                    <span className="service-name">Greca sencilla</span>
+                    <span className="service-price">$30</span>
                 </div>
+
                 <div className="container-cp" id="GrecaE" onClick={() => { dataC("GrecaE"); setIsOpen('dia') }}>
-                    <h3>Greca elaborada</h3>
-                    <p>$50</p>
-                </div>
-                <div className="container-cp" onClick={() => setIsOpen('corte')}>
-                    <h3>Combos</h3>
-                    <p></p>
+                    <span className="service-name">Greca elaborada</span>
+                    <span className="service-price">$50</span>
                 </div>
 
-                {isOpen === 'corte' && (
-                    <Modal1
-                        isOpen={isOpen}
-                        setIsOpen={setIsOpen}
-                        dataC={dataC}
-                    />
-                )}
-
-                {isOpen === 'dia' && (
-                    <Horario
-                        isOpen={isOpen}
-                        setIsOpen={setIsOpen}
-                        dataD={dataD}
-                    />
-                )}
-                {isOpen === 'present' && (
-                    <Presentacion
-                        isOpen={isOpen}
-                        setIsOpen={setIsOpen}
-                        datosCita={datosCita}
-                        env={env}
-                        setEnv={setEnv}
-                    />
-                )}
-                {isOpen === 'name' && (
-                    <Name
-                        isOpen={isOpen}
-                        setIsOpen={setIsOpen}
-                        dataUS={dataUS}
-                    />
-                )}
-
-
-                <p>Cada uno de estos servicios tiene un tiempo promedio de 50 minutos de duracion, haciendo exepcion en los combos</p>
+                <div className="container-cp combo-card" onClick={() => setIsOpen('corte')}>
+                    <span className="service-name">Combos</span>
+                    <span className="service-badge">Ver más</span>
+                </div>
             </div>
+
+            {/* Nota aclaratoria fuera del grid */}
+            <p className="service-note">
+                Cada uno de estos servicios tiene un tiempo promedio de 50 minutos de duración (excepto combos).
+            </p>
+
+            {/* Renderizado condicional de Modales (fuera de la cuadrícula) */}
+            {isOpen === 'corte' && <Modal1 isOpen={isOpen} setIsOpen={setIsOpen} dataC={dataC} />}
+            {isOpen === 'dia' && <Horario isOpen={isOpen} setIsOpen={setIsOpen} dataD={dataD} />}
+            {isOpen === 'present' && <Presentacion isOpen={isOpen} setIsOpen={setIsOpen} datosCita={datosCita} env={env} setEnv={setEnv} />}
+            {isOpen === 'name' && <Name isOpen={isOpen} setIsOpen={setIsOpen} dataUS={dataUS} />}
         </div>
 
     )
